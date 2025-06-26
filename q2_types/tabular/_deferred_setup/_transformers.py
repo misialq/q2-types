f5b166f88209be46694cef5b4957d294810ebbd1
Colin Wood <68213641+colinvwood@users.noreply.github.com>
2025-06-26 15:56:30 -0700

    IMP: Dataframe to jsonl changes (#361)

--- q2_types/tabular/_deferred_setup/_transformers.py
+++ q2_types/tabular/_deferred_setup/_transformers.py
@@ -21,6 +21,239 @@ from .. import (NDJSONFileFormat,
 from ...plugin_setup import plugin
 
 
+def _make_dtype_conversion(
+    df: pd.DataFrame, column: str, from_type: str, to_type: str
+):
+    '''
+    Converts the datatype of a column in a dataframe from `from_type` to
+    `to_type`. The `from_type` and `to_type` parameters may each be one of
+    'integer', 'number', 'string', 'boolean', 'datetime', or 'duration'.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe being converted to jsonl.
+    column : str
+        The name of the column in the dataframe that is having its dtype
+        converted.
+    from_type : str
+        The datatype of the column in the dataframe.
+    to_type : str
+        The datatype that the column should be converted to, if needed.
+
+    Returns
+    -------
+    None
+        Potentially changes the dtype of `column` in `df`.
+
+    Raises
+    ------
+    ValueError
+        If the conversion from `from_type` to `to_type` is not supported.
+    ValueError
+        If a string `from_type` can not be parsed into a datetime `to_type`.
+    ValueError
+        If a string `from_type` can not be parsed into a duration `to_type`.
+    '''
+    def _unsupported_conversion_error():
+        msg = (
+            f'The {column} column of type {from_type} can not be converted to '
+            f'the {to_type} type.'
+        )
+        raise ValueError(msg)
+
+    # cache column attrs
+    column_to_attrs = {}
+    for _column in df.columns:
+        column_to_attrs[_column] = df[_column].attrs
+
+    if from_type == 'datetime':
+        if to_type == 'datetime':
+            pass
+        elif to_type == 'date':
+            df[column] = df[column].dt.strftime('%Y-%m-%d').astype('string')
+        elif to_type == 'time':
+            df[column] = df[column].dt.strftime('%H:%M:%S').astype('string')
+        else:
+            _unsupported_conversion_error()
+    elif from_type == 'duration':
+        if to_type == 'duration':
+            df[column] = df[column].apply(pd.Timedelta.isoformat)
+        else:
+            _unsupported_conversion_error()
+    elif from_type == 'integer':
+        if to_type == 'integer':
+            pass
+        elif to_type == 'number':
+            pass
+        elif to_type == 'string':
+            df[column] = df[column].astype(str)
+        else:
+            _unsupported_conversion_error()
+    elif from_type == 'number':
+        if to_type == 'number':
+            pass
+        elif to_type == 'string':
+            df[column] = df[column].astype(str)
+        else:
+            _unsupported_conversion_error()
+    elif from_type == 'boolean':
+        if to_type == 'boolean':
+            pass
+        else:
+            _unsupported_conversion_error()
+    elif from_type == 'string':
+        if to_type in ('datetime', 'date', 'time'):
+            try:
+                df[column] = pd.to_datetime(df[column])
+            except ValueError:
+                msg = (
+                    f'The {column} column could not be parsed into the '
+                    'datetime representation.'
+                )
+                raise ValueError(msg)
+        elif to_type == 'duration':
+            try:
+                # NOTE: do we want to only accept iso-format duration strings?
+                df[column] = pd.to_timedelta(df[column])
+            except ValueError:
+                msg = (
+                    f'The {column} column could not be parsed into the '
+                    'timedelta representation.'
+                )
+                raise ValueError(msg)
+
+        if to_type == 'string':
+            pass
+        elif to_type == 'datetime':
+            df[column] = pd.to_datetime(df[column])
+        elif to_type == 'date':
+            df[column] = df[column].dt.strftime('%Y-%m-%d').astype('string')
+        elif to_type == 'time':
+            df[column] = df[column].dt.strftime('%H:%M:%S').astype('string')
+        elif to_type == 'duration':
+            df[column] = pd.to_timedelta(df[column])
+        else:
+            _unsupported_conversion_error()
+
+    # restore cached column attrs
+    for _column in df.columns:
+        df[_column].attrs = column_to_attrs[_column]
+
+
+def _get_dataframe_column_type(df: pd.DataFrame, column: str) -> str:
+    '''
+    Determines the conceptual jsonl datatype of `column` in `df`.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe that contains the column of interest.
+    column : str
+        The name of the column of interest.
+
+    Returns
+    -------
+    str
+        The determined conceptual datatype. One of 'integer', 'number',
+        'string', 'boolean', 'datetime', or 'duration'.
+
+    Raises
+    ------
+    ValueError
+        If `column` was not detected as any of the conceptual data types.
+    '''
+    if pd.api.types.is_integer_dtype(df[column]):
+        return 'integer'
+    if pd.api.types.is_float_dtype(df[column]):
+        return 'number'
+    if pd.api.types.is_string_dtype(df[column]):
+        return 'string'
+    if pd.api.types.is_bool_dtype(df[column]):
+        return 'boolean'
+    if pd.api.types.is_datetime64_dtype(df[column]):
+        return 'datetime'
+    if pd.api.types.is_timedelta64_dtype(df[column]):
+        return 'duration'
+
+    msg = (
+        f'The type of the {column} column was not detected as any of the '
+        'following types: integer, number, string, boolean, datetime, or '
+        'duration.'
+    )
+    raise ValueError(msg)
+
+
+def _copy_dataframe_with_attrs(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Creates and returns a copy of dataframe including any column attrs and
+    any dataframe-level attrs.
+    Preserves the column and dataframe attrs on the copied-from dataframe.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe to copy.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        The copied dataframe.
+    '''
+    df_attrs = df.attrs
+
+    column_to_attrs = {}
+    for column in df.columns:
+        column_to_attrs[column] = df[column].attrs
+
+    df_copy = df.copy()
+
+    df.attrs = df_attrs
+    df_copy.attrs = df_attrs
+
+    for column in df.columns:
+        df[column].attrs = column_to_attrs[column]
+        df_copy[column].attrs = column_to_attrs[column]
+
+    return df_copy
+
+
+def _process_dataframe_types(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Processes a dataframe by annotating columns with their inferred data types
+    where no annotation exists, and by converting columns to the annotated type
+    where the annotated and inferred types do not match.
+
+    Parameters
+    ----------
+    pd.DataFrame
+        The dataframe to process. This dataframe is not mutated.
+
+    Returns
+    -------
+    pd.DataFrame
+        A new dataframe with updated annotations and column types as necessary.
+    '''
+    df = _copy_dataframe_with_attrs(df)
+
+    for column in df:
+        try:
+            attr_type = df[column].attrs['type']
+        except KeyError:
+            attr_type = None
+
+        df_type = _get_dataframe_column_type(df, column)
+
+        if attr_type is None:
+            df[column].attrs['type'] = df_type
+        else:
+            _make_dtype_conversion(
+                df, column, from_type=df_type, to_type=attr_type
+            )
+
+    return df
+
+
 def table_jsonl_header(df: pd.DataFrame) -> str:
     header = {}
     header['doctype'] = dict(
@@ -31,16 +264,22 @@ def table_jsonl_header(df: pd.DataFrame) -> str:
     fields = []
     for name in df.columns:
         attrs = df[name].attrs.copy()
+        attr_type = attrs.pop('type', None)
         title = attrs.pop('title', '')
         description = attrs.pop('description', '')
-        type = attrs.pop('type', None)
         missing = attrs.pop('missing', False)
         extra = attrs.pop('extra', None)
         if extra is None:
             extra = attrs
+
         fields.append(dict(
-            name=name, type=type, missing=missing, title=title,
-            description=description, extra=extra))
+            name=name,
+            type=attr_type,
+            missing=missing,
+            title=title,
+            description=description,
+            extra=extra
+        ))
 
     header['fields'] = fields
     header['index'] = []
@@ -50,6 +289,23 @@ def table_jsonl_header(df: pd.DataFrame) -> str:
 
     # prevent whitespace after comma and colon
     return json.dumps(header, separators=(',', ':'))
+
+
+@plugin.register_transformer
+def df_to_table_jsonl(df: pd.DataFrame) -> TableJSONLFileFormat:
+    processed_df = _process_dataframe_types(df)
+    header = table_jsonl_header(processed_df)
+
+    ff = TableJSONLFileFormat()
+    with ff.open() as fh:
+        fh.write(header)
+        fh.write('\n')
+        if not processed_df.empty:
+            processed_df.to_json(
+                fh, orient='records', lines=True, date_format='iso'
+            )
+
+    return ff
 
 
 @plugin.register_transformer
@@ -74,13 +330,15 @@ def table_jsonl_to_df(ff: TableJSONLFileFormat) -> pd.DataFrame:
         elif spec['type'] == 'number':
             df[col] = df[col].astype('float64')
         elif spec['type'] == 'datetime':
-            df[col] = pd.to_datetime(df[col], format='iso8601')
+            df[col] = pd.to_datetime(df[col], format='ISO8601')
         elif spec['type'] == 'date':
-            df[col] = pd.to_datetime(df[col], format='iso8601')
+            df[col] = pd.to_datetime(df[col], format='ISO8601')
         elif spec['type'] == 'time':
             df[col] = pd.to_datetime(df[col], format='mixed').dt.time
         elif spec['type'] == 'duration':
             df[col] = pd.to_timedelta(df[col])
+        elif spec['type'] == 'string':
+            df[col] = df[col].astype('string')
 
     # 3. set index
     if len(header['index']) > 0:
@@ -95,20 +353,6 @@ def table_jsonl_to_df(ff: TableJSONLFileFormat) -> pd.DataFrame:
     df.attrs.update(attrs)
 
     return df
-
-
-@plugin.register_transformer
-def df_to_table_jsonl(obj: pd.DataFrame) -> TableJSONLFileFormat:
-    header = table_jsonl_header(obj)
-
-    ff = TableJSONLFileFormat()
-    with ff.open() as fh:
-        fh.write(header)
-        fh.write('\n')
-        if not obj.empty:
-            obj.to_json(fh, orient='records', lines=True, date_format='iso')
-
-    return ff
 
 
 @plugin.register_transformer
--- q2_types/tabular/tests/test_transformers.py
+++ q2_types/tabular/tests/test_transformers.py
@@ -5,8 +5,10 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+import json
 
 import pandas as pd
+from pandas.testing import assert_series_equal, assert_frame_equal
 
 from qiime2.plugin.testing import TestPluginBase
 from qiime2.plugin.util import transform
@@ -14,6 +16,9 @@ import qiime2.metadata
 
 from q2_types.tabular.formats import (
     TabularDataResourceDirFmt, TableJSONLFileFormat,
+)
+from q2_types.tabular._deferred_setup._transformers import (
+    _copy_dataframe_with_attrs
 )
 
 
@@ -68,5 +73,363 @@ class TestTransformers(TestPluginBase):
                                        'faithpd_refdist.table.jsonl')
         _, exp = self.transform_format(TableJSONLFileFormat, pd.DataFrame,
                                        'faithpd_refdist.table.jsonl')
+
         exp = exp.set_index('id')
-        pd.testing.assert_frame_equal(obs.to_dataframe(), exp)
+
+        obs = obs.to_dataframe()
+        obs.index = obs.index.astype('string')
+        obs['group'] = obs['group'].astype('string')
+
+        pd.testing.assert_frame_equal(obs, exp)
+
+
+class TestDataframeToJsonlTypeHandling(TestPluginBase):
+    package = 'q2_types.tabular.tests'
+
+    def setUp(self):
+        '''
+        Creates a dataframe with a column for each of the recognized dataframe
+        types for use in the other tests in this class.
+        '''
+        super().setUp()
+
+        self.df = pd.DataFrame({
+            'integer_column': [1, 2, 3],
+            'float_column': [1.0, 2.5, 3.0],
+            'string_column': ['i', 'like', 'strings'],
+            'datetime_column': pd.to_datetime(
+                ['2012-01-01T00:00:00', '17:00:00', '1998-03-14'],
+                format='mixed'
+            ),
+            'timedelta_column': pd.to_timedelta(
+                ['P0D5H', 'P420D', 'P7D24H60M60S']
+            )
+        })
+
+    def get_header_field(self, header: dict, name: str) -> dict:
+        for field in header['fields']:
+            if field['name'] == name:
+                return field
+
+        raise ValueError(f'The {name} field was not found in the header.')
+
+    def test_that_dummy_df_has_intended_types(self):
+        '''
+        Tests that the columns in the pandas dataframe created in the `setUp`
+        have the intended types. (Does not test any plugin behavior.)
+        '''
+        type_assertion_funcs = {
+            'integer': pd.api.types.is_integer_dtype,
+            'float': pd.api.types.is_float_dtype,
+            'string': pd.api.types.is_string_dtype,
+            'datetime': pd.api.types.is_datetime64_dtype,
+            'timedelta': pd.api.types.is_timedelta64_dtype,
+        }
+
+        for column in self.df.columns:
+            column_type = column.removesuffix('_column')
+
+            assert_func = type_assertion_funcs[column_type]
+            self.assertTrue(assert_func(self.df[column]))
+
+            for other_column_type in type_assertion_funcs:
+                if other_column_type != column_type:
+                    assert_not_func = type_assertion_funcs[other_column_type]
+                    self.assertFalse(assert_not_func(self.df[column]))
+
+    def test_attrs_written_to_jsonl_header(self):
+        '''
+        Tests that dataframe column attrs are written to the jsonl header
+        properly.
+        '''
+        self.df['integer_column'].attrs['type'] = 'integer'
+        self.df['float_column'].attrs['type'] = 'number'
+        self.df['string_column'].attrs['type'] = 'string'
+        self.df['datetime_column'].attrs['type'] = 'datetime'
+        self.df['timedelta_column'].attrs['type'] = 'duration'
+
+        jsonl = transform(self.df, to_type=TableJSONLFileFormat)
+
+        with open(jsonl.path, 'r') as fh:
+            header_line = fh.readline()
+            header_dict = json.loads(header_line)
+
+        for column in self.df.columns:
+            field = self.get_header_field(header_dict, column)
+            self.assertEqual(field['type'], self.df[column].attrs['type'])
+
+    def test_invalid_attr_type_errors(self):
+        '''
+        Tests that columns with invalid type attrs produce an error.
+        '''
+        self.df['integer_column'].attrs['type'] = 'datetime'
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r'.*type integer can not be converted.*datetime.*'
+        ):
+            transform(self.df, to_type=TableJSONLFileFormat)
+
+    def test_missing_attrs_inferred_properly(self):
+        '''
+        Tests that dataframe columns that do not have attrs have their type
+        properly inferred and written to the jsonl header.
+        '''
+        column_to_jsonl_type = {
+            'integer_column': 'integer',
+            'float_column': 'number',
+            'string_column': 'string',
+            'datetime_column': 'datetime',
+            'timedelta_column': 'duration',
+        }
+
+        jsonl = transform(self.df, to_type=TableJSONLFileFormat)
+
+        with open(jsonl.path, 'r') as fh:
+            header_line = fh.readline()
+            header_dict = json.loads(header_line)
+
+        for column in self.df.columns:
+            field = self.get_header_field(header_dict, column)
+            self.assertEqual(field['type'], column_to_jsonl_type[column])
+
+    def test_invalid_dataframe_column_type_errors(self):
+        '''
+        Tests that a column of a type that is not in the set of supported
+        data types produces an error.
+        '''
+        self.df['float_column'] = self.df['float_column'].astype('category')
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r'.*type of the float_column column was not detected as any of.*'
+        ):
+            transform(self.df, to_type=TableJSONLFileFormat)
+
+    def assert_proper_type_conversion(
+        self,
+        df: pd.DataFrame,
+        column: str,
+        expected_column: pd.Series
+    ):
+        '''
+        Transforms a dataframe to and back from the jsonl format then asserts
+        that a column of interest is stored as expected.
+        '''
+        jsonl = transform(df, to_type=TableJSONLFileFormat)
+        round_trip_df = transform(jsonl, to_type=pd.DataFrame)
+
+        expected_column.name = column
+        assert_series_equal(round_trip_df[column], expected_column)
+
+    def test_integer_type_conversions(self):
+        '''
+        Tests that a column of the integer type can be converted to the number
+        and string types.
+        '''
+        self.df['integer_column'].attrs['type'] = 'number'
+        self.assert_proper_type_conversion(
+            self.df,
+            'integer_column',
+            pd.Series([1.0, 2.0, 3.0], dtype='float64')
+        )
+
+        self.df['integer_column'].attrs['type'] = 'string'
+        self.assert_proper_type_conversion(
+            self.df,
+            'integer_column',
+            pd.Series(['1', '2', '3'], dtype='string')
+        )
+
+    def test_float_type_conversions(self):
+        '''
+        Tests that a column of the float type can be converted to the number
+        type.
+        '''
+        self.df['float_column'].attrs['type'] = 'string'
+        self.assert_proper_type_conversion(
+            self.df,
+            'float_column',
+            pd.Series(['1.0', '2.5', '3.0'], dtype='string')
+        )
+
+    def test_string_type_conversions(self):
+        '''
+        Tests that columns of the string type can be converted to the datetime,
+        date, time, and duration types, whether in the corresponding formats
+        or not.
+        '''
+        df = pd.DataFrame({
+            'datetime': ['2025-06-02T13:00:00'],
+            'date': ['2025-06-02'],
+            'time': ['13:00:00'],
+            'duration': ['P7D7H7M7S'],
+        })
+
+        # immediately corresponding formats
+        df['datetime'].attrs['type'] = 'datetime'
+        self.assert_proper_type_conversion(
+            df,
+            'datetime',
+            pd.Series(
+                [pd.to_datetime('2025-06-02T13:00:00')],
+                dtype='datetime64[ns]'
+            )
+        )
+
+        df['date'].attrs['type'] = 'date'
+        self.assert_proper_type_conversion(
+            df,
+            'date',
+            pd.Series(
+                pd.to_datetime(['2025-06-02T00:00:00']),
+                dtype='datetime64[ns]'
+            )
+        )
+
+        df['time'].attrs['type'] = 'time'
+        self.assert_proper_type_conversion(
+            df,
+            'time',
+            pd.to_datetime(pd.Series(['13:00:00'])).dt.time
+        )
+
+        df['duration'].attrs['type'] = 'duration'
+        self.assert_proper_type_conversion(
+            df,
+            'duration',
+            pd.Series(pd.to_timedelta(['P7D7H7M7S']), dtype='timedelta64[ns]')
+        )
+
+        # non immediately corresponding formats
+        # NOTE: not all possible conversions are enumerated here
+        df['datetime'].attrs['type'] = 'time'
+        self.assert_proper_type_conversion(
+            df,
+            'datetime',
+            pd.to_datetime(pd.Series(['13:00:00'])).dt.time
+        )
+
+        df['date'].attrs['type'] = 'datetime'
+        self.assert_proper_type_conversion(
+            df,
+            'date',
+            pd.Series(
+                pd.to_datetime(['2025-06-02T00:00:00']), dtype='datetime64[ns]'
+            )
+        )
+
+        df['time'].attrs['type'] = 'datetime'
+        self.assert_proper_type_conversion(
+            df,
+            'time',
+            pd.Series(
+                pd.to_datetime(['13:00:00']), dtype='datetime64[ns]'
+            )
+        )
+
+    def test_datetime_type_conversions(self):
+        '''
+        Tests that a column of the datetime type can be converted to the
+        date and time types.
+        '''
+        self.df['datetime_column'].attrs['type'] = 'date'
+        self.assert_proper_type_conversion(
+            self.df,
+            'datetime_column',
+            pd.Series(
+                pd.to_datetime([
+                    '2012-01-01T00:00:00', '00:00:00', '1998-03-14T00:00:00'
+                ], format='mixed'),
+                dtype='datetime64[ns]'
+            )
+        )
+
+        self.df['datetime_column'].attrs['type'] = 'time'
+        self.assert_proper_type_conversion(
+            self.df,
+            'datetime_column',
+            pd.to_datetime(
+                pd.Series(['00:00:00', '17:00:00', '00:00:00'])
+            ).dt.time
+        )
+
+    def test_boolean_type_round_trips(self):
+        '''
+        Tests that columns of the dtype bool are successfully round-tripped,
+        whether the type is annotated in the attrs, or inferred. Note that
+        the boolean type is not tested alongside the other types because
+        it is not convertable to any other types and no other types convert to
+        it.
+        '''
+        df = pd.DataFrame({
+            'bool': [True, False],
+            'crip': [False, True],
+        })
+        df['bool'].attrs['type'] = 'boolean'
+
+        jsonl = transform(df, to_type=TableJSONLFileFormat)
+        round_trip_df = transform(jsonl, to_type=pd.DataFrame)
+
+        assert_frame_equal(df, round_trip_df)
+
+    def test_copy_dataframe_with_attrs(self):
+        '''
+        Tests that the `_copy_dataframe_with_attrs` function accurately
+        copies a dataframe and its column and dataframe-level attributes, and
+        that it preserves these attributes in the copied-from dataframe.
+        '''
+        self.df.attrs = {'yay': 'attrs'}
+        self.df['integer_column'].attrs = {'key': 'value'}
+        self.df['string_column'].attrs = {'oompa': 'loompa'}
+
+        copied_df = _copy_dataframe_with_attrs(self.df)
+
+        assert_frame_equal(copied_df, self.df)
+
+        self.assertEqual(copied_df.attrs, {'yay': 'attrs'})
+        self.assertEqual(copied_df['integer_column'].attrs, {'key': 'value'})
+        self.assertEqual(copied_df['string_column'].attrs, {'oompa': 'loompa'})
+
+        self.assertEqual(self.df.attrs, {'yay': 'attrs'})
+        self.assertEqual(self.df['integer_column'].attrs, {'key': 'value'})
+        self.assertEqual(self.df['string_column'].attrs, {'oompa': 'loompa'})
+
+    def test_json_representations(self):
+        '''
+        Tests that the json representations of various data types are as
+        they are expected to be.
+        '''
+        self.df['boolean_column'] = [True, False, True]
+
+        jsonl = transform(self.df, to_type=TableJSONLFileFormat)
+        with open(str(jsonl)) as fh:
+            json_content = fh.read()
+
+        self.assertIn('"integer_column":1', json_content)
+        self.assertIn('"float_column":1.0', json_content)
+        self.assertIn('"string_column":"strings"', json_content)
+        self.assertIn('"boolean_column":true', json_content)
+        self.assertIn(
+            '"datetime_column":"2012-01-01T00:00:00.000"', json_content
+        )
+        self.assertIn(
+            '"datetime_column":"1998-03-14T00:00:00.000"', json_content
+        )
+        self.assertIn('"timedelta_column":"P420DT0H0M0S"', json_content)
+
+        # datetime to date
+        self.df['datetime_column'].attrs['type'] = 'date'
+        jsonl = transform(self.df, to_type=TableJSONLFileFormat)
+        with open(str(jsonl)) as fh:
+            json_content = fh.read()
+
+        self.assertIn('"datetime_column":"1998-03-14"', json_content)
+
+        # datetime to time
+        self.df['datetime_column'].attrs['type'] = 'time'
+        jsonl = transform(self.df, to_type=TableJSONLFileFormat)
+        with open(str(jsonl)) as fh:
+            json_content = fh.read()
+
+        self.assertIn('"datetime_column":"17:00:00"', json_content)
